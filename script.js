f3974562978bb31c35e3f57423e4eec836d20832
srishti <srishtiprasad500@gmail.com>
2022-01-25 10:08:43 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const inputElement=document.getElementById("lbsInput");
+        inputElement.addEventListener('input',function(e){
+            let lbs=e.target.value;
+            document.getElementById("gramsOutput").innerHTML=lbs/0.0022046;
+            document.getElementById("kgOutput").innerHTML=lbs/0.2046;
+            document.getElementById("ozOutput").innerHTML=lbs*16;
+        })
